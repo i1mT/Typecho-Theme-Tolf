6bfac026254494777084e30d5ec77354ed0050dd
tfh93121 <tfhhh@qq.com>
2017-03-01 23:02:30 +0800

    优化文章页图片处理

--- js/post_js.js
+++ js/post_js.js
@@ -25,34 +25,37 @@ $(document).ready(function(){
       }
       relatePages.css('height',max);
     }
-    //下面的代码将文章中的图片大小优化一下  后7个
-    var contentImg = $('article').find('img');//获取文章中所有的图片
-    var articleWidth = $('article').width();
-    for (var i = 0; i < contentImg.length-7; i++) {
-      if( $(contentImg[i]).width()>= articleWidth*0.9 && $(contentImg[i]).height()>articleWidth*0.3){
+    window.onload = setImg;
+    function setImg(){
+      //下面的代码将文章中的图片大小优化一下  后7个
+      var contentImg = $('article').find('img');//获取文章中所有的图片
+      var articleWidth = $('article').width();
+      for (var i = 0; i < contentImg.length-8; i++) {
+        if( $(contentImg[i]).width()>= articleWidth*0.9){
+            $(contentImg[i]).css({
+              width: '104%',
+              margin: '.7rem -2% 0 -2%',
+            })
+        }else if( $(contentImg[i]).width()>= articleWidth*0.4 ){
+          var width = '50%';
+          if($(contentImg[i]).width()>= articleWidth*0.5 ){width = '60%';}
+          var marginCenter = ((100-parseInt(width))/2)+'%';
           $(contentImg[i]).css({
-            width: '104%',
-            margin: '.7rem -2% 0 -2%',
+            width: width,
+            'margin-top': '.7rem',
+            'margin-right': marginCenter,
+            'margin-bottom': '0',
+            'margin-left': marginCenter
           })
-      }else if( $(contentImg[i]).width()>= articleWidth*0.4 ){
-        var width = '50%';
-        if($(contentImg[i]).width()>= articleWidth*0.5 ){width = '60%';}
-        var marginCenter = ((100-parseInt(width))/2)+'%';
-        $(contentImg[i]).css({
-          width: width,
-          'margin-top': '.7rem',
-          'margin-right': marginCenter,
-          'margin-bottom': '0',
-          'margin-left': marginCenter
-        })
-        var img_alt = '<div class="img-alt">'+ contentImg[i].alt +'</div>';
-        $(contentImg[i].parentNode).css('text-align','center');
-        $(contentImg[i].parentNode).append(img_alt);
-      }else{
-        $(contentImg[i]).css({
-          width: '30%',
-          margin: '.7rem 35% 0 35%'
-        })
+          var img_alt = '<div class="img-alt">'+ contentImg[i].alt +'</div>';
+          $(contentImg[i].parentNode).css('text-align','center');
+          $(contentImg[i].parentNode).append(img_alt);
+        }else{
+          $(contentImg[i]).css({
+            width: '30%',
+            margin: '.7rem 35% 0 35%'
+          })
+        }
       }
     }
   })
